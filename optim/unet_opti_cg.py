a23e4ffe689fbc3b7d9cf110dba48cc6596efd8a
NouamaneTazi <nouamane98@gmail.com>
2022-08-31 09:02:18 +0000

    it seems cuda graph doesn't boost perf for unet

--- optim/unet_opti_cg.py
+++ optim/unet_opti_cg.py
@@ -11,7 +11,16 @@ t = joblib.load('t.pkl')
 text_embeddings = joblib.load('text_embeddings.pkl')
 
 unet_path = '/home/nouamane_huggingface_co/.cache/huggingface/diffusers/models--CompVis--stable-diffusion-v1-3/snapshots/c0399c1dac67eb30c20b40886872cee2fdf2e6b6/unet'
+# unet_path = '/home/nouamane_huggingface_co/.cache/huggingface/diffusers/models--CompVis--stable-diffusion-v1-3/snapshots/3bcaa468131c963401aa5175a14b13912b9f1933/unet' # fp16
 unet = UNet2DConditionModel.from_pretrained(unet_path).cuda()
+
+
+with torch.no_grad():
+    for _ in range(3):
+        start_time = time.time()
+        noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings)["sample"]
+        print(noise_pred.var())
+        print(f"Pipeline inference took {time.time() - start_time} seconds")
 
 
 # warmup
@@ -30,11 +39,19 @@ with torch.cuda.graph(g):
         noise_pred = unet(latent_model_input, t, encoder_hidden_states=text_embeddings)["sample"]
 
 print(noise_pred.var())
-start_time = time.time()
-for _ in range(10):
+for _ in range(3):
+    start_time = time.time()
     g.replay()
-print(noise_pred.var())
-print(noise_pred.var())
-print(f"Pipeline inference took {time.time() - start_time} seconds")
-time.sleep(2)
-print(noise_pred.var())
+    print(noise_pred.var())
+    print(f"Pipeline inference took (CG) {time.time() - start_time} seconds")
+
+# with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+#         # schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+#         on_trace_ready=tensorboard_trace_handler(f"./tb_logs/tb_unet_CG_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"),
+#         record_shapes=True,
+#         profile_memory=False,
+#         with_stack=True
+#         ) as prof:
+
+#     g.replay()
+#     print(noise_pred.var())
