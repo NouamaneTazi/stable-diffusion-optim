a5c6e89aee8b6a2ebe02ec6243b80d876c3757a3
Nouamane Tazi <nouamane98@gmail.com>
2022-09-13 22:36:06 +0000

    autocast

--- optim/baseline.py
+++ optim/baseline.py
@@ -5,7 +5,6 @@ from torch.profiler import profile, record_function, ProfilerActivity, tensorboa
 import datetime
 import time
 import torch
-
 scheduler = LMSDiscreteScheduler(
     beta_start=0.00085, 
     beta_end=0.012, 
@@ -16,27 +15,29 @@ pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-3", 
     scheduler=scheduler,
     use_auth_token=True,
-    # revision="fp16",
+    revision="fp16",
     # torch_dtype=torch.float16
 ).to("cuda")
 
 prompt = "a photo of an astronaut riding a horse on mars"
 
 # warmup
-image = pipe(prompt, num_inference_steps=8)["sample"][0]  
+with autocast("cuda"):
+    image = pipe(prompt, num_inference_steps=8)["sample"][0]  
 
 for _ in range(3):
     torch.cuda.synchronize()
     start_time = time.time()
     with torch.inference_mode():
-        image = pipe(prompt, num_inference_steps=50)["sample"][0]  
+        with autocast("cuda"):
+            image = pipe(prompt, num_inference_steps=50)["sample"][0]  
     torch.cuda.synchronize()
     print(f"Pipeline inference took {time.time() - start_time:.2f} seconds")
 image.save(f"pics/pic_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png")
 
 with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
         # schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
-        on_trace_ready=tensorboard_trace_handler(f"./tb_logs/tb_pt_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"),
+        on_trace_ready=tensorboard_trace_handler(f"./tb_logs/tb_pt_autocast_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"),
         record_shapes=True,
         profile_memory=False,
         with_stack=True
@@ -45,7 +46,8 @@ with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
     torch.cuda.synchronize()
     start_time = time.time()
     with torch.inference_mode():
-        image = pipe(prompt, num_inference_steps=8)["sample"][0]  
+        with autocast("cuda"):
+            image = pipe(prompt, num_inference_steps=8)["sample"][0]  
     torch.cuda.synchronize()
     print(f"Pipeline inference took (w/ Profiler) {time.time() - start_time:.2f} seconds")
         
